c98da0725cd79e6aadb6037e7c3c2645c6b39033
unknown <navdeepdhiman91@gmail.com>
2024-08-02 18:58:24 +0530

    new changes

---
+++ src/overloading/overloadingDemo.java
@@ -0,0 +1,30 @@
+package overloading;
+
+public class overloadingDemo {
+	
+	public void Sample() {
+		
+		System.out.println("Inside of method SampleOne");
+		 
+	}
+
+	public void Sample(int a) {
+		
+		System.out.println("Inside sample one");
+	}
+	
+	public void Sample(int a, int b) {
+		
+		System.out.println("Inside sample one");
+		
+	}
+		
+	public void Sample(int a, double b) {
+		
+		System.out.println("Inside sample one");
+		
+	}
+	
+	
+	
+}
